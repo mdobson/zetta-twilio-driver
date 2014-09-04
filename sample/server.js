6c04d8dfb80c610e536eda970904535165421360
Matt Dobson <mdobson4@gmail.com>
2014-09-04 09:23:46 -0400

    Updates to sample.

--- sample/server.js
+++ sample/server.js
@@ -1,6 +1,8 @@
 var zetta = require('zetta');
 var TwilioDriver = require('../');
 var AutoScout = require('zetta-auto-scout');
+var SonosDriver = require('zetta-sonos-driver');
+var app = require('./app');
 
 var twilioOpts = {
   phoneNumber:'+17342452497'
@@ -11,5 +13,7 @@ var Twilio = new AutoScout('phone', TwilioDriver, twilioOpts);
 zetta()
   .name('matt.dobson')
   .use(Twilio)
+  .use(SonosDriver)
+  .load(app)
   .listen(1337);
 
